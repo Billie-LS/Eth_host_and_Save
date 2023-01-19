99670c4762d0327e5b76b0f5258ecb8f27b82fa1
Billie-LS <lskylizard@yahoo.com>
2023-01-18 19:16:17 -0600

    code update and notation

--- joint_savings.sol
+++ joint_savings.sol
@@ -11,18 +11,18 @@ a solidity smart contract that
 
 pragma solidity ^0.5.0;
 
-// Define a new contract named `JointSavings`
+// Define new contract named `JointSavings`
 contract JointSavings {
 
-    // Store the addresses of the account owners
+    // Store addresses of the account owners
     address payable accountOne;
     address payable accountTwo;
 
-    // Keep track of the last account to withdraw, and the amount withdrawn
+    // Keep track of last account to withdraw 'lastToWithdraw', and last amount withdrawn 'lastWithdrawAmount'
     address public lastToWithdraw;
     uint public  lastWithdrawAmount;
 
-    // Keep track of the contract's balance
+    // Keep track of contract's balance 'contractBalance'
     uint public contractBalance;
 
     /**
@@ -38,15 +38,15 @@ contract JointSavings {
         // Ensures that the contract has enough balance for the withdraw operation
         require(address(this).balance >= amount, "Insufficient funds!");
 
-        // Checks if the last account to withdraw is different from the current recipient
+        // Checks if last account to withdraw is different from current recipient
         if (lastToWithdraw != recipient) {
             lastToWithdraw = recipient;
         }
 
-        // Transfers the specified amount to the recipient
+        // Transfers specified amount to recipient
         recipient.transfer(amount);
 
-        // Updates the last withdraw amount and the contract balance
+        // Updates last withdraw amount and contract balance
         lastWithdrawAmount = amount;              // Set  `lastWithdrawAmount` equal to `amount`
         contractBalance = address(this).balance;  // Update the contract balance
     }
@@ -57,13 +57,13 @@ contract JointSavings {
     }
 
     /**
-     * Sets the accountOne and accountTwo addresses
+     * Sets accountOne and accountTwo addresses
      * @param account1 The address of accountOne
      * @param account2 The address of accountTwo
      */
     function setAccounts(address payable account1, address payable account2) public{
 
-        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
+        // Set values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
         accountOne = account1;
         accountTwo = account2;
     }
